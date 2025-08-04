36b74ea913e6b98150ad623253247b964a1e6dc4
gyozasushi <denisabizar54@gmail.com>
2025-08-05 06:41:30 +0700

    refactor(home): clean up layout and improve code readability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,33 +3,29 @@ import HeroText from "@/components/homepage/HeroText"
 import CatImageWithEyes from "@/components/homepage/ImageCatEyes"
 import Carousel from "@/components/homepage/Carousel"
 import HeroText2 from "@/components/homepage/HeroText2"
-import { carousellist } from "@/components/homepage/Corousellist"
 import HeroText3 from "@/components/homepage/HeroText3"
+import { carousellist } from "@/components/homepage/Corousellist"
 
 export default function Home({ eyeRefs }) {
   const [selectedCat, setSelectedCat] = useState(carousellist[0])
 
   return (
     <div className="min-h-screen">
-      {/* section 1 */}
-      <section className="flex flex-col px-6 pt-20 sm:space-x-10 pb-20 border-b-4">
+      {/* Section 1: Hero */}
+      <section className="flex flex-col sm:flex-row px-6 pt-20 pb-20 border-b-4 sm:space-x-10">
         <HeroText />
         <CatImageWithEyes eyeRefs={eyeRefs} />
       </section>
 
-      {/* section 2 */}
-      <section className="flex flex-col pb-20 pt-10 bg-secondary-background border-b-4 ">
-        <div className="">
-          <Carousel onSlideChange={setSelectedCat} />
-        </div>
-        <div>
-          <HeroText2 
-          cat={selectedCat}
-          key={selectedCat.id}/>
-        </div>
+      {/* Section 2: Carousel & Info */}
+      <section className="flex flex-col pb-20 pt-10 bg-secondary-background border-b-4">
+        <Carousel onSlideChange={setSelectedCat} />
+        <HeroText2 cat={selectedCat} />
       </section>
-      <section className="bg-background flex sm:flex-row pb-10 ">
-        <HeroText3/>
+
+      {/* Section 3: Additional Info */}
+      <section className="bg-background flex sm:flex-row pb-10">
+        <HeroText3 />
       </section>
     </div>
   )
